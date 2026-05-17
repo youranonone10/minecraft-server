ee29d2a97513b17b581a2ee233a80ca27c533a22
Aman Singh <103933063+youranonone10@users.noreply.github.com>
2026-05-17 17:25:33 +0530

    Download Paper server at runtime if missing

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -e
 
 # Accept EULA
 echo "eula=true" > eula.txt
+
+# Download Paper if missing
+if [ ! -f server.jar ]; then
+  echo "Downloading Paper server..."
+  curl -fsSL -o server.jar https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/562/downloads/paper-1.20.4-562.jar
+fi
 
 # Start the server with JVM optimizations
 java -Xmx${RAM:-1024}M -Xms${RAM:-1024}M \
